b0b443758a909b845793af0711b58e7b82d1cc40
bzt <zoltan.baldaszti@gmail.com>
2016-08-28 20:59:54 +0100

    .

--- vendor/phppe/og/libs/OpenGraph.php
+++ vendor/phppe/og/libs/OpenGraph.php
@@ -5,14 +5,14 @@
  * @date 28 Aug 2016
  * @brief
  */
-namespace PHPPE;
 
+namespace PHPPE;
 use PHPPE\Core as Core;
 use PHPPE\View as View;
 use PHPPE\Http as Http;
-ude PHPPE\DB as DB;
+use PHPPE\DB as DB;
 
-class OpenGraph extends PHPPE\Extension
+class OpenGraph extends \PHPPE\Extension
 {
 	public function ctrl($app, $method)
 	{
